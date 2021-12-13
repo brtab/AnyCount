aa475e7d0db8e04585b89c08e735cc5da1bca912
Brian <brtabarez@alumno.unlam.edu.ar>
2021-12-13 11:00:37 -0300

    Dialog de categoria

--- app/src/main/java/com/example/aplicacioncuentas/MainActivity.kt
+++ app/src/main/java/com/example/aplicacioncuentas/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aplicacioncuentas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.aplicacioncuentas.databinding.ActivityMainBinding
@@ -16,7 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         binding.fabAdd.setOnClickListener {
-
+            intent = Intent(this, CreateActivity::class.java)
+            startActivity(intent)
         }
     }
 }
